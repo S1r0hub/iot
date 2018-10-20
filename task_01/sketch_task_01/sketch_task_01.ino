b850a45eaff6516916cbb0a0fc39a23257cd383a
S1r0hub <lionkill1ger@gmail.com>
2018-10-20 16:38:13 +0200

    added other kind of drop catch animation and player death animation

--- task_01/sketch_task_01/sketch_task_01.ino
+++ task_01/sketch_task_01/sketch_task_01.ino
@@ -11,22 +11,26 @@ const uint8_t grid_x = 5;
 const uint8_t grid_y = 5;
 
 int player_score = 0;
-int player_x = 2;
-int player_y = 4;
+int player_x = 0;
+int player_y = 0;
 bool player_left = false;
 bool player_right = false;
 bool showScore = false;
+bool game_end = false;
+
+uint8_t decrease_after = 2;
+int last_score_decrease = 0;
 
 bool object_alive = false;
 int object_x = 0;
-int object_x_spawn = 2;
 int object_y = 0;
 bool randomObjectX = false; // enable/disable random x position
 
 // how many ticks the object waits until falling down
-int object_wait_move = 5;
-int object_wait_death = 5;
-int object_wait_killed = 10; // how long to wait if player killed it
+int object_wait_move = 0;
+const int object_wait_move_init = 50;
+const int object_wait_death = 50;
+const int object_wait_killed = 100; // how long to wait if player killed it
 int object_cur = 0; // timer that decreases
 
 uint8_t gamescreen[] =
@@ -40,16 +44,9 @@ uint8_t gamescreen[] =
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Microbit is ready?!");
 
   pinMode(buttonA, INPUT);
   pinMode(buttonB, INPUT);
-
-  // set initial values
-  object_alive = false;
-  object_y = 0;
-  object_x = object_x_spawn;
-  object_cur = object_wait_move;
 
   // set random seed
   // if nothing connected to this pin,
@@ -57,26 +54,79 @@ void setup() {
   randomSeed(analogRead(0));
 
   microbit.begin();
-  microbit.show(microbit.HEART);
+  Serial.println("Microbit ready!");
   delay(1000);
+
+  // set initial values
+  resetGame(true);
 }
 
 void loop() {
 
-  if (!showScore) {
-    CheckPlayerMovement();
+  // wait for user to press a button to restart
+  if (game_end) {
+    if (!digitalRead(buttonA) || !digitalRead(buttonB)) {
+      resetGame(false);
+      delay(1000);
+    }
   }
-  ObjectUpdate();
+  else {
+    // update player and object
+    if (!showScore) {
+      CheckPlayerMovement();
+      ObjectUpdate();
+    }
+  }
+  
+  // render scene
   RenderScene();
-
-  delay(100);
+  delay(10);
 }
 
+
+// reset the game to initial state
+void resetGame(bool firstStart) {
+
+  object_y = 0;
+  object_x = 2;
+  object_wait_move = object_wait_move_init; // initial value
+  object_cur = object_wait_move;
+  object_alive = false;
+
+  player_x = 2;
+  player_y = 4;
+  player_score = 0;
+  player_left = false;
+  player_right = false;
+  
+  showScore = false;
+  
+  if (firstStart) {
+    microbit.show(microbit.HEART);
+    game_end = true;
+  }
+  else {
+    game_end = false;
+    Serial.println("Game reset!");
+    
+    microbit.clear();
+    delay(500);
+    
+    // count down
+    for (uint8_t i = 0; i < 3; i++) {
+      microbit.print(3-i);
+      delay(1000);  
+    }
+    microbit.clear();
+  }
+}
+
+
+// check if the player wants to move
 void CheckPlayerMovement() {
   if (!digitalRead(buttonA)) {
     if (!player_left) {
       player_left = true;
-      Serial.println("Left");
       MoveLeft();
     }
   }
@@ -87,7 +137,6 @@ void CheckPlayerMovement() {
   if (!digitalRead(buttonB)) {
     if (!player_right) {
       player_right = true;
-      Serial.println("Right");
       MoveRight();
     }
   }
@@ -96,12 +145,16 @@ void CheckPlayerMovement() {
   }
 }
 
+
+// make player move left
 void MoveLeft() {
   if (player_x < grid_x-1) {
     player_x++;;
   }
 }
 
+
+// make player move right
 void MoveRight() {
   if (player_x > 0) {
     player_x--;
@@ -109,6 +162,7 @@ void MoveRight() {
 }
 
 
+// update object position and status
 void ObjectUpdate() {
 
   object_cur--;
@@ -124,6 +178,8 @@ void ObjectUpdate() {
   }
 }
 
+
+// spawn the object
 void spawnObject() {
 
   object_cur = object_wait_move;
@@ -133,21 +189,121 @@ void spawnObject() {
 
   // randomly select spawn location
   object_x = getRandomXSpawn();
+
+  // increase speed using eulers number
+  // (remove less from wait time the more the score increases)
+  int remove_wait = speedWaitRemove(player_score);
+  if (player_score > 0 && remove_wait > 0) {
+    object_wait_move -= remove_wait;
+    Serial.print("Decreasing wait time with ");
+    Serial.print(remove_wait);
+    Serial.print(" at ");
+    Serial.println(object_wait_move);
+  }
+
   object_alive = true;
-  showScore = false;
+  Serial.println("New object spawned!");
 }
 
+
+// Get how much to remove from the wait time depending on player score
+// to increase the current object movement speed
+int speedWaitRemove(int current_score) {
+  
+  // decrease each x points if greater than minimum
+  if (current_score - last_score_decrease > decrease_after-1) {
+    last_score_decrease = current_score;
+
+    if (object_wait_move > object_wait_move_init * 0.8) {
+      return 5;
+    }
+
+    // wait longer until increasing speed
+    decrease_after = 3;
+
+    if (object_wait_move > object_wait_move_init * 0.5) {
+      return 2;
+    }
+
+    // wait longer until increasing speed
+    decrease_after = 4;
+    
+    if (object_wait_move > 5) {
+      return 1;
+    }
+  }
+  
+  return 0;
+}
+
+
+// called when the player hits the object
 void killObject(int playerPoints) {
 
+  Serial.println("Object catched!");
   player_score += playerPoints;
   object_cur = object_wait_death;
+  object_alive = false;
+  
   if (playerPoints > 0) {
     object_cur = object_wait_killed;
-    showScore = true;
+    delay(20);
+
+    // catch object sequence
+    for (int y = player_y; y >= 0; y--) {
+      uint8_t curRow = gamescreen[y];
+      curRow |= 1 << player_x;
+      gamescreen[y] = curRow;
+      microbit.show(gamescreen);
+      delay(20);
+    }
   }
-  object_alive = false;
 }
 
+
+// called when the player did not catch the object
+void killPlayer() {
+
+  Serial.println("GAME OVER!");
+  object_alive = false;
+
+  // play end sequence
+  bool dir = true;
+  for (uint8_t y = 0; y < grid_y; y++) {
+    uint8_t curRow = gamescreen[y];
+    
+    for (uint8_t x = 0; x < grid_x; x++) {
+      uint8_t shiftVal = x;
+      if (!dir) { shiftVal = grid_x - x - 1; }
+      curRow |= 1 << shiftVal;
+      gamescreen[y] = curRow;
+      microbit.show(gamescreen);
+      delay(50);
+    }
+
+    // change direction
+    dir = !dir;
+  }
+  delay(100);
+
+  // flashing effect
+  for (uint8_t i = 0; i < 3; i++) {
+    microbit.clear();
+    delay(100);
+    microbit.show(gamescreen);
+    delay(100);
+  }
+
+  // finally clear and wait a moment
+  microbit.clear();
+  delay(500);
+
+  // show score until player presses a button to reset
+  showScore = true;
+}
+
+
+// update object position
 void moveObject() {
 
   object_cur = object_wait_move;
@@ -160,13 +316,13 @@ void moveObject() {
 
   object_y++;
   if (object_y == player_y && object_x == player_x) {
+    
     // player kills object and gets a point
     killObject(1);
   }
   else if (object_y > grid_y-1) {
     // player didn't catch object
-    // TODO: should the player lose?
-    killObject(0);
+    killPlayer();
   }
 }
 
@@ -176,12 +332,13 @@ int getRandomXSpawn() {
   return random(0, grid_x);
 }
 
+
 // Get next or previous value of the given randomly.
 int getRandomX(int curX) {
-  int dir = -1;
-  if (random(0,10) < 5) {
-    dir = 1;
-  }
+  int dir = 0;
+  int rndNum = random(0,15);
+  if (rndNum < 10) { dir = 1; }
+  if (rndNum < 5) { dir = -1; }
   int newX = curX + dir;
   if (newX >= 0 && newX < grid_x) {
     return newX;
@@ -190,35 +347,41 @@ int getRandomX(int curX) {
 }
 
 
+// render the player, object and score
 void RenderScene() {
 
+  // show player score
   if (showScore) {
     microbit.print(player_score);
 
-    // reset instantly because
-    // printing such numbers takes longer
-    if (player_score > 9) {
-      object_cur = 0;
+    // game over
+    if (!game_end) {
+      game_end = true;
     }
+  }
+
+  // don't render the scene if game is over
+  if (game_end) {
+    delay(100);
     return;
   }
 
+  // clear game screen
   for (uint8_t y = 0; y < grid_y; y++) {
     uint8_t curRow = B00000;
-
-    // render player
-    if (y == player_y) {
-      curRow |= 1 << player_x;
-    }
-
-    // render object
-    if (y == object_y) {
-      curRow |= 1 << object_x;
-    }
-
     gamescreen[y] = curRow;
   }
-  
+
+  // add player position
+  if (player_y > -1 && player_y < grid_y) {
+    gamescreen[player_y] = gamescreen[player_y] | 1 << player_x;
+  }
+
+  // add object position
+  if (object_alive && object_y > -1 && object_y < grid_y) {
+    gamescreen[object_y] = gamescreen[object_y] | 1 << object_x;
+  }
+
+  // show the scene
   microbit.show(gamescreen);
-  
 }
