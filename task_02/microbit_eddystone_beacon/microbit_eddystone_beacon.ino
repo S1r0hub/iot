d025aff3d8d395ff2f9b1b2756c3dd9f2d447308
S1r0hub <lionkill1ger@gmail.com>
2018-11-27 18:26:26 +0100

    added selection of r,p or s; added showing selection; added broadcasting selection (somehow...) for 10 seconds

--- task_02/microbit_eddystone_beacon/microbit_eddystone_beacon.ino
+++ task_02/microbit_eddystone_beacon/microbit_eddystone_beacon.ino
@@ -14,6 +14,9 @@
 #include <Adafruit_Microbit.h>
 Adafruit_Microbit microbit;
 
+const int buttonA = 5;
+const int buttonB = 11;
+
 // For Accelerometer: https://learn.adafruit.com/user-micro-bit-with-arduino/accelerometer-and-magnetometer
 // Library download: https://cdn-learn.adafruit.com/assets/assets/000/046/217/original/MMA8653.zip
 #include "Wire.h"
@@ -34,84 +37,221 @@ MMA8653 accel;
 EddystoneBeacon eddystoneBeacon = EddystoneBeacon(EDDYSTONE_BEACON_REQ, EDDYSTONE_BEACON_RDY, EDDYSTONE_BEACON_RST);
 int i = 0;
 
+// when to broadcast and how long
+bool broadcasting = false;
+const float broadcastTime = 10; // in seconds
+unsigned long broadcasting_start = 0;
+
+// time in ms for acceleration measurement
+const int diffTime = 60;
+float valx_p = 0;
+float valy_p = 0;
+float valz_p = 0;
+
+// stores changes of acceleration
+byte shakeByte = B00000000;
+
+// last time a shake occured
+unsigned long lastShakeTime = 0;
+uint8_t shakesDone = 0;
+
+// max amount of time in ms to wait between shakes
+const float maxShakeWait = 1200;
+const uint8_t shakesToSend = 2;
+
+// rock, paper, scissor selection
+uint8_t selection = 0;
+const uint8_t selections = 3;
+bool buttonA_pressed = false;
+bool buttonB_pressed = false;
+
+const char* rps_names[] = {
+  "Rock",
+  "Paper",
+  "Scissor"
+};
+
+const uint8_t rps[3][5] = {
+  {
+    B11100,
+    B10010,
+    B11100,
+    B10100,
+    B10010
+  },
+  {
+    B11100,
+    B10010,
+    B11100,
+    B10000,
+    B10000
+  },
+  {
+    B01110,
+    B10000,
+    B01100,
+    B00010,
+    B11100
+  }
+};
+
+const uint8_t check[] = {
+  B00001,
+  B00011,
+  B10011,
+  B11110,
+  B01100
+};
+
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
   // power = broadcasting power / transmit power
   // (usually between -40 and +4 dBm)
-  eddystoneBeacon.begin(-20, "Initialized"); // power, URI
+  eddystoneBeacon.begin(-18, ""); // power, URI
   Serial.println(F("Eddystone URL Beacon"));
   
   // Start LED matrix driver after radio (required)
   microbit.begin();
 
+  // show initial selection on matrix display
+  showSelection();
+
+  // register micro bit buttons
+  pinMode(buttonA, INPUT);
+  pinMode(buttonB, INPUT);
+
   // 8-bit mode, 2g range
   accel.begin(false, 2);
 }
 
-//int amin = 9999999;
-//int amax = -9999999;
-
-int diffTime = 100;
-float valx_p = 0;
-float valy_p = 0;
-float valz_p = 0;
-
-//float ms[10];
-byte shakeByte = B00000000;
 
 void loop() {
 
-  /*
-  i++;
-  char* out = "Rock";
-  if (i % 2 == 0) { 
-    out = "The";
-    microbit.matrix.print(out);
+  // show rock, paper or scissor
+  if (!broadcasting) {
+
+    // handle button press and show selection
+    handleButtons();
+
+    // check if user shaked the micro bit
+    checkForShake();
+
+    // check if user did enough shakes and send
+    if (shakesDone >= shakesToSend) {
+      shakesDone = 0;
+      broadcasting = true;
+      broadcasting_start = millis();
+
+      eddystoneBeacon.setURI(rps_names[selection]);
+      //eddystoneBeacon.begin(-18, rps_names[selection]); // power, URI
+      Serial.print("Eddystone Beacon begin with: ");
+      Serial.println(rps_names[selection]);
+      
+      microbit.matrix.show(check);
+      delay(100);
+    }
   }
-  */
+
+  // directly enter the beacon loop
+  if (broadcasting) {
+    unsigned long curTime = millis();
+
+    // end broadcast
+    if (curTime > broadcasting_start + broadcastTime * 1000) {
+      broadcasting = false;
+      //eddystoneBeacon.end(); // calling end will not allow to start it again... :(
+      eddystoneBeacon.setURI("");
+      Serial.println("Broadcast end!");
+      showSelection();
+    }
+    else {
+      eddystoneBeacon.loop();
+    }
+  }
+}
+
+
+// read button inputs and set variables accordingly
+void handleButtons() {
+
+  bool aPressed = !digitalRead(buttonA);
+  bool bPressed = !digitalRead(buttonB);
+
+  if (aPressed && !buttonA_pressed) { buttonA_pressed = true; buttonAClick(); }
+  else if (!aPressed && buttonA_pressed) { buttonA_pressed = false; }
+
+  if (bPressed && !buttonB_pressed) { buttonB_pressed = true; buttonBClick(); }
+  else if (!bPressed && buttonB_pressed) { buttonB_pressed = false; }
+}
+
+
+// handle clicks of button A
+void buttonAClick() {
+  changeSelection(-1);
+}
+
+
+// handle clicks of button B
+void buttonBClick() {
+  changeSelection(1);
+}
+
+
+// change r, p, s selection accordingly
+void changeSelection(int it) {
+
+  int nextSelection = selection + it;
+  if (nextSelection < 0) { nextSelection = selections-1; }
+  else if (nextSelection > selections-1) { nextSelection = 0; }
+
+  // apply new selection
+  selection = (uint8_t) nextSelection;
+  Serial.print("Selection changed to: "); Serial.println(selection);
   
-  //eddystoneBeacon.setURI(out);
-  //eddystoneBeacon.loop();
-  //delay(10);
+  // show selection on matrix
+  showSelection();
+}
 
 
-  // accel test
+// shows selection on matrix
+void showSelection() {
+  microbit.matrix.show(rps[selection]);
+}
+
+
+// shows the shakes as well
+void showShakesDone() {
+  for (uint i = 0; i < 5; i++) {
+    uint16_t col = LED_OFF;
+    if (shakesDone > i) { col = LED_ON; }
+    microbit.matrix.drawPixel(4, 4-i, col);
+  }
+}
+
+
+// Check if the user shaked the device.
+// A shake will trigger broadcasting the selection
+// for the specified amount of time.
+void checkForShake() {
+  
+  // use accelerometer
   accel.update();
   float valx = accel.getX();
   float valy = accel.getY();
   float valz = accel.getZ();
 
-  //if (val < amin) { amin = val; }
-  //if (val > amax) { amax = val; }
-  
-  //Serial.print("Accel Z: ");
-  //Serial.println(val);
-  //Serial.print("Min: "); Serial.print(amin); Serial.print(", Max: "); Serial.print(amax); Serial.print(", Cur: "); Serial.println(val);
-
+  // calculate acceleration
   float accel_speed = abs(valx + valy + valz - valx_p - valy_p - valz_p) / float(diffTime);
-
   //Serial.print("accel_speed: "); Serial.println(accel_speed);
 
-  /*
-  for (int i = 9; i > 0; i--) {
-    ms[i] = ms[i-1];
-  }
-  ms[0] = accel_speed;
-
-  for (int i = 0; i < 10; i++) {
-    Serial.print(ms[i] > 1.0);
-    Serial.print(", ");
-  }
-  Serial.println();
-  */
-
+  // "free" space for the next bit
   shakeByte = shakeByte >> 1;
 
-  // store 1 as bit at most left byte position (128 = 10000000)
-  // if the threshold is reached
-  if (accel_speed > 0.7) {
+  // store 1 as bit at most left byte position (128 = 10000000) if threshold is exceeded
+  if (accel_speed > 0.65) {
     shakeByte = shakeByte | 128;
   }
   
@@ -122,31 +262,55 @@ void loop() {
   int breakTime = 0;
   for (int i = 0; i < 8; i++) {
     bool thisVal = shakeByte >> i & 1;
-    Serial.print(thisVal);
-    Serial.print(", ");
+    //Serial.print(thisVal);
+    //Serial.print(", ");
 
     if (thisVal) {
       if (!gotUp) {
-        gotUp = true; 
+        gotUp = true;
+        //Serial.println("Got up");
       }
       
       if (gotBreak && !gotDown) {
         gotDown = true;
+        //Serial.println("Got down");
       }
     }
     else {
       if (gotUp && !gotBreak) {
         breakTime++;
-        if (breakTime > 1) {
+        //Serial.print("Break: "); Serial.println(breakTime);
+        if (breakTime > 3) {
           gotBreak = true;
+          //Serial.println("Got Break!");
         }
       }
     }
   }
-  Serial.println();
+  //Serial.println();
 
   if (gotUp && gotBreak && gotDown) {
     Serial.println("Shake!");
+
+    unsigned long curTime = millis();
+    if (curTime > lastShakeTime + maxShakeWait) {
+      shakesDone = 1;
+    }
+    else {
+      shakesDone += 1;
+    }
+
+    // print shake info
+    Serial.print("Shakes done: ");
+    Serial.print(shakesDone);
+    Serial.print("/");
+    Serial.print(shakesToSend);
+    Serial.print(" (Time diff: ");
+    Serial.print((curTime - lastShakeTime) / 1000.0);
+    Serial.println(" seconds)");
+
+    // store last shake time
+    lastShakeTime = curTime;
 
     // reset the byte data
     shakeByte = 0;
@@ -155,6 +319,9 @@ void loop() {
   valx_p = valx;
   valy_p = valy;
   valz_p = valz;
-  
+
   delay(diffTime);
+
+  // update matrix depending on shakes done
+  //showShakesDone();
 }
