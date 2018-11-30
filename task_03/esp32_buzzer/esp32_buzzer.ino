f5a831c35d73fbe5a44585078fbd005094658ae9
S1r0hub <lionkill1ger@gmail.com>
2018-11-30 14:33:31 +0100

    function

--- task_03/esp32_buzzer/esp32_buzzer.ino
+++ task_03/esp32_buzzer/esp32_buzzer.ino
@@ -1,3 +1,6 @@
+// We can not use analogWrite easily because its not supported by ESP32...
+// So we can't use the "tone" function as well..
+
 const uint8_t BUZZ = 12;
 
 void setup() {
@@ -11,18 +14,34 @@ void setup() {
 void loop() {
 
   // active buzzer frequency output
-  for (int i = 0; i < 80; i++) {
-    digitalWrite(BUZZ, HIGH);
-    delay(1);
-    digitalWrite(BUZZ, LOW);
-    delay(1);
-  }
+  playTone(100, 1, 1);
+  delay(2000);
 
-  for (int i = 0; i < 100; i++) {
-    digitalWrite(BUZZ, HIGH);
-    delay(2);
-    digitalWrite(BUZZ, LOW);
-    delay(2);
-  }
+  // lower pitch
+  playTone(100, 5, 2);
+  delay(2000);
+
+  // high pitch
+  playTone(100, 2, 5);
+  delay(2000);
+
+  // lower pitch but louder
+  playTone(50, 10, 1);
+  delay(2000);
+
+  // high pitch and low volume
+  playTone(50, 1, 10);
+  delay(2000);
+
+  delay(3000);
   
+}
+
+void playTone(int toneLength, int delay1, int delay2) {
+  for (int i = 0; i < toneLength; i++) {
+    digitalWrite(BUZZ, HIGH);
+    delay(delay1);
+    digitalWrite(BUZZ, LOW);
+    delay(delay2);
+  }
 }
